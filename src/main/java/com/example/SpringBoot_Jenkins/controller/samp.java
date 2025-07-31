fe0b679ddf1635b37fafc28a06ffb087ea57e759
AditMagilHub <adit.khurana@magilhub.com>
2025-07-31 10:51:46 +0530

    first commit

---
+++ src/main/java/com/example/SpringBoot_Jenkins/controller/samp.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_Jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class samp {
+
+    @GetMapping("/hello")
+    public String Hello(){
+        return "Hello World";
+    }
+}
